756bf7e65216a919b11eeb370f815e1eea6aea01
Ankit Kumar <60535576+ankitkumar0021@users.noreply.github.com>
2025-07-11 09:17:29 +0530

    new changes

--- src/components/widgets/banners/MainBanner.tsx
+++ src/components/widgets/banners/MainBanner.tsx
@@ -12,9 +12,9 @@ import ImageComponent from "@/components/ui/ImageComponent";
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 const banners = [
-  { id: 1, image: "/images/common/banner1.png", link: "#", alt: "Banner 1", text: "Welcome to Our Site" },
-  { id: 2, image: "/images/common/banner6.png", link: "#", alt: "Banner 2", text: "Discover Amazing Deals" },
-  { id: 3, image: "/images/common/banner5.png", link: "#", alt: "Banner 3", text: "Join Us Today" },
+  { id: 1, image: "/images/common/banner1.png", link: "#", alt: "Banner 1"  },
+  { id: 2, image: "/images/common/banner6.png", link: "#", alt: "Banner 2" },
+  { id: 3, image: "/images/common/banner5.png", link: "#", alt: "Banner 3" },
 ];
 
 function chunkArray<T>(arr: T[], size: number): T[][] {
@@ -28,38 +28,44 @@ function chunkArray<T>(arr: T[], size: number): T[][] {
 export default function MainBanner() {
   const slides = chunkArray(banners, 1);
 
-  return (
-    <div className="relative main_banner bg_gradient">
-      <div className="w-full pb-2">
-        <Swiper
-          modules={[Navigation, Pagination, Autoplay, A11y]}
-          spaceBetween={10}
-          slidesPerView={1}
-          loop={true}
-          autoplay={{ delay: 5000 }}
-          pagination={{ clickable: true }}
-          navigation={{ nextEl: ".custom-next", prevEl: ".custom-prev" }}
-          className="w-full"
-        >
-          {slides.map((group, index) => (
-            <SwiperSlide key={index}>
-              <div className="grid grid-cols-1 w-full lg:h-[600px]">
-                {group.map((banner) => (
-                  <Link href={banner.link} key={banner.id} className="relative w-full h-full">
-                    <ImageComponent url={banner.image} img_title={banner.alt} object_cover={true} />
-                    {/* Animated Text Overlay */}
-                    <div className="absolute inset-0 flex items-center justify-center">
-                      <h2 className="text-white md:text-6xl font-bold">{banner.text}</h2>
-                    </div>
-                  </Link>
-                ))}
-              </div>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-        <button className="custom-prev absolute top-1/2 left-2 z-10 -translate-y-1/2 text-white text-3xl"><IoIosArrowBack className="!text-3xl"/></button>
-        <button className="custom-next absolute top-1/2 right-2 z-10 -translate-y-1/2 text-white text-3xl"><IoIosArrowForward className="!text-3xl"/></button>
-      </div>
-    </div>
-  );
+   return (
+        <>
+            <div className="relative main_banner bg_gradient">
+                <div className="w-full pb-2">
+                    <Swiper
+                        modules={[Navigation, Pagination, Autoplay, A11y]}
+                        spaceBetween={10}
+                        slidesPerView={1}
+                        loop={true}
+                        autoplay={{ delay: 5000 }}
+                        pagination={{ clickable: true }}
+                        navigation={{
+                            nextEl: ".custom-next",
+                            prevEl: ".custom-prev",
+                        }}
+                        className="w-full"
+                    >
+                        {slides.map((group, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="grid grid-cols-1 w-full h-[500px]">
+                                    {group.map((banner) => (
+                                        <Link href={banner.link} key={banner.id} className="relative w-full">
+                                            <ImageComponent url={banner.image} img_title={banner.alt} object_cover={true} />
+                                        </Link>
+                                    ))}
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+
+                    <button className="custom-prev absolute top-1/2 left-2 z-10 -translate-y-1/2 text-white text-3xl">
+                        <IoIosArrowBack className="!text-3xl" />
+                    </button>
+                    <button className="custom-next absolute top-1/2 right-2 z-10 -translate-y-1/2 text-white text-3xl">
+                        <IoIosArrowForward className="!text-3xl" />
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
